7533654500f5b4b481ffd3a84c8dd5c6566fc050
BENLABEDdjahid <2716157@collegelacite.ca>
2024-07-28 05:36:25 +0100

    components

---
+++ components/header/header.js
@@ -0,0 +1,20 @@
+import "./header.css";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className="header">
+      <div >
+         <Link href= "/" className="logo"> Benlabed Djahid </Link>
+      </div>
+      <nav className="nav-links">
+        <Link href="/">Accueil</Link>
+        <Link href="/projets">Projets</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/reviews"> Témoignages</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
