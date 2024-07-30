ca20c38e2bc08b499a652158985e467e71096f41
BENLABEDdjahid <2716157@collegelacite.ca>
2024-07-30 11:00:56 +0100

    mod

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ix7bdsvdvMnt+0sYYw7+ktcC3UkbtFbo9NwgITZkVTA=\"\n}"
--- README.md
+++
@@ -1,1 +0,0 @@
-# portfolio-Djahid-Benlabed
