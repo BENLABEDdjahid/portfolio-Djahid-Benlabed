429c6025ec45c6b08458afe5e4bce2ce87fb00d3
BENLABEDdjahid <2716157@collegelacite.ca>
2024-07-30 11:08:08 +0100

    comm

---
+++ portfolio-Djahid-Benlabed/components/footer/footer.js
@@ -0,0 +1,16 @@
+import "./footer.css";
+import Link from "next/link";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      
+      <Link href="https://github.com/BENLABEDdjahid">GitHub</Link>
+      <Link href="https://www.linkedin.com/in/djahid-benlabed-496817296/">
+        LinkedIn
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
